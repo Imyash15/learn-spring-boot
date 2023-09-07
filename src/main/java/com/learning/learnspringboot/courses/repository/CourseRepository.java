b884ab40ee5d5042a8dbb7efaf4ef2976389758c
Yash <yash@byteCipher.net>
2023-09-07 11:33:20 +0530

    Repository

--- src/main/java/com/learning/learnspringboot/courses/repository/CourseRepository.java
+++ src/main/java/com/learning/learnspringboot/courses/repository/CourseRepository.java
@@ -3,7 +3,9 @@ package com.learning.learnspringboot.courses.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learning.learnspringboot.courses.bean.Course;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
